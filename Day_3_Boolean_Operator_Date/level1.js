ec9d4ac1765d6879f85dc1b64c3b491518c69748
HoHuuAn <anho.work@gmail.com>
2023-04-29 23:59:53 +0700

    Day3

---
+++ Day_3_Boolean_Operator_Date/level1.js
@@ -0,0 +1,88 @@
+// Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+const firstName = 'An'
+const lastName = 'Ho'
+const country = 'VietNam'
+const city = 'Ho Chi Minh city'
+const age = 20
+const isMarried = false
+const year = 2023
+console.log("Ex1:")
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
+console.log(typeof year)
+
+// Check if type of '10' is equal to 10
+console.log("Ex2:")
+console.log(typeof ('10' == 10))
+
+// Check if parseInt('9.8') is equal to 10
+console.log("Ex3:")
+console.log((parseInt('9.8') == 10))
+
+// Boolean value is either true or false
+// Write three JavaScript statement which provide truthy value
+console.log("Ex4:")
+console.log(10 == 10)
+console.log(5 == 5)
+console.log(1 == 1)
+// Write three JavaScript statement which provide falsy value
+console.log(10 == 9)
+console.log(8 == 9)
+console.log(1 == 9)
+
+// Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+console.log("Ex5:")
+// 4 > 3
+console.log(4 > 3)
+// 4 >= 3
+console.log(4 >= 3)
+// 4 < 3
+console.log(4 < 3)
+// 4 <= 3
+console.log(4 <= 3)
+// 4 == 4
+console.log(4 == 4)
+// 4 === 4
+console.log(4 === 4)
+// 4 != 4
+console.log(4 != 4)
+// 4 !== 4
+console.log(4 !== 4)
+// 4 != '4'
+console.log(4 != '4')
+// 4 == '4'
+console.log(4 == '4')
+// 4 === '4'
+console.log(4 === '4')
+// Find the length of python and jargon and make a falsy comparison statement.
+console.log('python' === 'jargon')
+
+// Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+console.log("Ex6:")
+// 4 > 3 && 10 < 12 true
+
+// 4 > 3 && 10 > 12 false
+// 4 > 3 || 10 < 12
+// 4 > 3 || 10 > 12
+// !(4 > 3)
+// !(4 < 3)
+// !(false)
+// !(4 > 3 && 10 < 12)
+// !(4 > 3 && 10 > 12)
+// !(4 === '4')
+// There is no 'on' in both dragon and python
+
+// Use the Date object to do the following activities
+console.log("Ex7:")
+
+// What is the year today?
+// What is the month today as a number?
+// What is the date today?
+// What is the day today as a number?
+// What is the hours now?
+// What is the minutes now?
+// Find out the numbers of seconds elapsed from January 1, 1970 to now.
